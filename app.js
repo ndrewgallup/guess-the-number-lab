69be87a8a3040bfefe60e8be88612ad77d4c91b1
AndrewG1993 <andrew.ryan.gallop@gmail.com>
2022-01-29 13:58:22 -0600

    complete Task 5

--- app.js
+++ app.js
@@ -19,16 +19,31 @@ const game = {
     
     do { //do...while loop with parseInt prompt while the guess is not a number or less than 1 or greater than 100
       guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}:`))
-    } while (
-      isNan(guess) || guess < this.smallestNum || guess > this.biggestNum
+    } while (isNan(guess) || guess < this.smallestNum || guess > this.biggestNum
     )
     
     return guess;
   },
   
   //this is Task 5
+  render: function() {
+    let message
 
+    //if...the player guesses correctly 
+    if (this.prevGuesses[this.prevGuesses.length -1] === this.secretNum) {
+      message = `Congrats! You guessed the number in ${this.prevGuesses.length} ${this.prevGuesses.length > 1 ? 'guesses' : 'guess'}!` //returns 'guesses' or 'guess' depending on prevGuesses.length
+    } else {
+      message = `Your guess is too ${this.prevGuesses[this.prevGuesses.length -1] > this.secretNum ? "high" : "low"}. \nPrevious guesses: ${this.prevGuesses.join(', ')}`
+    }
+
+    return message;
   },
 
+  }
 
-console.log(game.play())
+
+
+
+
+
+  console.log(game.play())
